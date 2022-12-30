01cfe2a80cc993600b8913fc1288daa5c0764bf9
ZhangHaHei <zzsperson@163.com>
2022-12-30 18:56:11 +0800

    complete Lab02

--- hw/hw02/hw02.py
+++ hw/hw02/hw02.py
@@ -1,4 +1,4 @@
-from operator import add, mul, sub
+from operator import add, mul
 
 
 def square(x): return x * x
@@ -18,8 +18,9 @@ HW_SOURCE_FILE = __file__
 
 def product(n, term):
     """Return the product of the first n terms in a sequence.
-    n -- a positive integer
-    term -- a function that takes one argument to produce the term
+
+    n: a positive integer
+    term:  a function that takes one argument to produce the term
 
     >>> product(3, identity)  # 1 * 2 * 3
     6
@@ -37,13 +38,13 @@ def product(n, term):
     "*** YOUR CODE HERE ***"
     sum = 1
     for i in range(n):
-        sum = sum * term(i+1)
+        sum = sum * term(i + 1)
     return sum
 
 
-def accumulate(combiner, base, n, term):
-    """Return the result of combining the first n terms in a sequence and base.
-    The terms to be combined are term(1), term(2), ..., term(n).  combiner is a
+def accumulate(merger, start, n, term):
+    """Return the result of merging the first n terms in a sequence and start.
+    The terms to be merged are term(1), term(2), ..., term(n). merger is a
     two-argument commutative function.
 
     >>> accumulate(add, 0, 5, identity)  # 0 + 1 + 2 + 3 + 4 + 5
@@ -56,148 +57,53 @@ def accumulate(combiner, base, n, term):
     25
     >>> accumulate(mul, 2, 3, square)    # 2 * 1^2 * 2^2 * 3^2
     72
+    >>> # 2 + (1^2 + 1) + (2^2 + 1) + (3^2 + 1)
     >>> accumulate(lambda x, y: x + y + 1, 2, 3, square)
     19
-    >>> accumulate(lambda x, y: 2 * (x + y), 2, 3, square)
-    58
+    >>> # ((2 * 1^2 * 2) * 2^2 * 2) * 3^2 * 2
+    >>> accumulate(lambda x, y: 2 * x * y, 2, 3, square)
+    576
     >>> accumulate(lambda x, y: (x + y) % 17, 19, 20, square)
     16
     """
     "*** YOUR CODE HERE ***"
-    ans, i = base, 1
-    while i <= n:
-        ans, i = combiner(ans, term(i)), i+1
-    return ans
+    sum = start
+    for i in range(n):
+        sum = merger(sum, term(i+1))
+    return sum
 
 
 def summation_using_accumulate(n, term):
-    """Returns the sum of term(1) + ... + term(n). The implementation
-    uses accumulate.
+    """Returns the sum: term(0) + ... + term(n), using accumulate.
 
     >>> summation_using_accumulate(5, square)
     55
     >>> summation_using_accumulate(5, triple)
     45
-    >>> from construct_check import check
-    >>> # ban iteration and recursion
-    >>> check(HW_SOURCE_FILE, 'summation_using_accumulate',
-    ...       ['Recursion', 'For', 'While'])
-    True
+    >>> # You aren't expected to understand the code of this test.
+    >>> # Check that the bodies of the functions are just return statements.
+    >>> # If this errors, make sure you have removed the "***YOUR CODE HERE***".
+    >>> import inspect, ast
+    >>> [type(x).__name__ for x in ast.parse(inspect.getsource(summation_using_accumulate)).body[0].body]
+    ['Expr', 'Return']
     """
-    "*** YOUR CODE HERE ***"
-
+    # "*** YOUR CODE HERE ***"
     return accumulate(add, 0, n, term)
 
 
 def product_using_accumulate(n, term):
-    """An implementation of product using accumulate.
+    """Returns the product: term(1) * ... * term(n), using accumulate.
 
     >>> product_using_accumulate(4, square)
     576
     >>> product_using_accumulate(6, triple)
     524880
-    >>> from construct_check import check
-    >>> # ban iteration and recursion
-    >>> check(HW_SOURCE_FILE, 'product_using_accumulate',
-    ...       ['Recursion', 'For', 'While'])
-    True
+    >>> # You aren't expected to understand the code of this test.
+    >>> # Check that the bodies of the functions are just return statements.
+    >>> # If this errors, make sure you have removed the "***YOUR CODE HERE***".
+    >>> import inspect, ast
+    >>> [type(x).__name__ for x in ast.parse(inspect.getsource(product_using_accumulate)).body[0].body]
+    ['Expr', 'Return']
     """
-    "*** YOUR CODE HERE ***"
-    return(mul, 1, n, term)
-
-
-def compose1(func1, func2):
-    """Return a function f, such that f(x) = func1(func2(x))."""
-    def f(x):
-        return func1(func2(x))
-    return f
-
-
-def make_repeater(func, n):
-    """Return the function that computes the nth application of func.
-
-    >>> add_three = make_repeater(increment, 3)
-    >>> add_three(5)
-    8
-    >>> make_repeater(triple, 5)(1) # 3 * 3 * 3 * 3 * 3 * 1
-    243
-    >>> make_repeater(square, 2)(5) # square(square(5))
-    625
-    >>> make_repeater(square, 4)(5) # square(square(square(square(5))))
-    152587890625
-    >>> make_repeater(square, 0)(5) # Yes, it makes sense to apply the function zero times!
-    5
-    """
-    "*** YOUR CODE HERE ***"
-    return accumulate(compose1, identity, n, lambda x: func)
-
-
-print(make_repeater(triple, 5)(1))
-
-
-def zero(f):
-    return lambda x: x
-
-
-def successor(n):
-    return lambda f: lambda x: f(n(f)(x))
-
-
-def one(f):
-    """Church numeral 1: same as successor(zero)"""
-    "*** YOUR CODE HERE ***"
-
-
-def two(f):
-    """Church numeral 2: same as successor(successor(zero))"""
-    "*** YOUR CODE HERE ***"
-
-
-three = successor(two)
-
-
-def church_to_int(n):
-    """Convert the Church numeral n to a Python integer.
-
-    >>> church_to_int(zero)
-    0
-    >>> church_to_int(one)
-    1
-    >>> church_to_int(two)
-    2
-    >>> church_to_int(three)
-    3
-    """
-    "*** YOUR CODE HERE ***"
-
-
-def add_church(m, n):
-    """Return the Church numeral for m + n, for Church numerals m and n.
-
-    >>> church_to_int(add_church(two, three))
-    5
-    """
-    "*** YOUR CODE HERE ***"
-
-
-def mul_church(m, n):
-    """Return the Church numeral for m * n, for Church numerals m and n.
-
-    >>> four = successor(three)
-    >>> church_to_int(mul_church(two, three))
-    6
-    >>> church_to_int(mul_church(three, four))
-    12
-    """
-    "*** YOUR CODE HERE ***"
-
-
-def pow_church(m, n):
-    """Return the Church numeral m ** n, for Church numerals m and n.
-
-    >>> church_to_int(pow_church(two, three))
-    8
-    >>> church_to_int(pow_church(three, two))
-    9
-    """
-    "*** YOUR CODE HERE ***"
+    # "*** YOUR CODE HERE ***"
+    return accumulate(mul, 1, n, term)
